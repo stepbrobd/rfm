54d6dffca791e858073f10b6e577f44a5578dfdc
Yifei Sun <ysun@hey.com>
2026-03-14 20:41:18 +0100

    bpf/include/vmlinux: clang format

--- cmd/bpfgen/testdata/with.bpf.c
+++ cmd/bpfgen/testdata/with.bpf.c
@@ -2,9 +2,10 @@
 #include <bpf/bpf_helpers.h>
 
 SEC("tp/raw_syscalls/sys_enter")
-int handle_sys_enter(void *ctx) {
-  bpf_printk("sys_enter");
-  return 0;
+int handle_sys_enter(void *ctx)
+{
+	bpf_printk("sys_enter");
+	return 0;
 }
 
 char LICENSE[] SEC("license") = "GPL";
--- cmd/bpfgen/testdata/without.bpf.c
+++ cmd/bpfgen/testdata/without.bpf.c
@@ -3,6 +3,9 @@
 #define SEC(name) __attribute__((section(name), used))
 
 SEC("tp/raw_syscalls/sys_enter")
-int handle_sys_enter(void *ctx) { return 0; }
+int handle_sys_enter(void *ctx)
+{
+	return 0;
+}
 
 char LICENSE[] SEC("license") = "GPL";
