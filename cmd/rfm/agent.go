bad8036c286ac7d10021b1450c932249ace42925
Yifei Sun <ysun@hey.com>
2026-03-15 12:38:07 +0100

    agent: wire probe, reader, and collector runtime

--- cmd/rfm/agent.go
+++ cmd/rfm/agent.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/spf13/cobra"
+	"ysun.co/rfm/collector"
+	"ysun.co/rfm/probe"
 )
 
 var agentCmd = &cobra.Command{
@@ -17,5 +22,28 @@ func init() {
 }
 
 func runAgent(cmd *cobra.Command, args []string) error {
-	return fmt.Errorf("agent mode is not implemented yet")
+	p, err := probe.Load(probe.Config{SampleRate: 100})
+	if err != nil {
+		return fmt.Errorf("load probe: %w", err)
+	}
+	defer p.Close()
+
+	// TODO: accept ifindex from flag or config
+	ifindex := 1
+	if err := p.Attach(ifindex); err != nil {
+		return fmt.Errorf("attach: %w", err)
+	}
+
+	rd, err := collector.NewReader(p.FlowEvents(), p.FlowDrops())
+	if err != nil {
+		return fmt.Errorf("open reader: %w", err)
+	}
+	defer rd.Close()
+
+	c := collector.New(30 * time.Second)
+
+	ctx, cancel := signal.NotifyContext(cmd.Context(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	return c.Run(ctx, rd)
 }
